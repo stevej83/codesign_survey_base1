46983f4d9409ff71a84ca21b4de89471132bebbf
Steve JIN <jinyue306@gmail.com>
2017-03-28 18:17:19 +0800

    no progress

--- src/SurveyBase1/SurveyBase1/Controllers/SurveyController.cs
+++ src/SurveyBase1/SurveyBase1/Controllers/SurveyController.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SurveyBase1.Models;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace SurveyBase1.Controllers
 {
     public class SurveyController : Controller
     {
-        // GET: Survey
         public ActionResult Landing()
         {
             return View();
         }
+
 
         public ActionResult Workflow()
         {
---
+++ src/SurveyBase1/SurveyBase1/Models/Survey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SurveyBase1.Models
+{
+    public class Survey
+    {
+        public int SurveyID { get; set; }
+        public int OrganizationID { get; set; }
+        public DateTime SurveyStartDateTime { get; set; }
+    }
+
+    public class SurveyDBContext : DbContext
+    {
+        public DbSet<Survey> Surveys { get; set; }
+    }
+}
