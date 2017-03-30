e660a82d065c3f8f989836067c81270fdc489340
Steve JIN <jinyue306@gmail.com>
2017-03-30 11:02:59 +0800

    web form constratuction

---
+++ src/SurveyBase1/SurveyBase1/Models/Survey.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurveyBase1.Models
+{
+    public class Survey
+    {
+        [ScaffoldColumn(false)]
+        public int SurveyID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int ClientID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int UserID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime SurveyStart { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime SurveyEnd { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime SurveyLastUpdate { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Q1 Answer")]
+        public string Q1Ans { get; set; }
+
+        [Display(Name = "Q1 Point")]
+        public double? Q1Pt { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Q2 Answer")]
+        public string Q2Ans { get; set; }
+
+        [Display(Name = "Q2 Point")]
+        public double? Q2Pt { get; set; }
+    }
+}
---
+++ src/SurveyBase1/SurveyBase1/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurveyBase1.Models
+{
+    public class User
+    {
+        [ScaffoldColumn(false)]
+        public int UserID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Wechat")]
+        public string Wechat { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DOB { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool IsBachelor { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Bachelor University")]
+        public string BachelorUniversity { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Bachelor Degree Detail")]
+        public string BachelorDegreeDetail { get; set; }
+    }
+}
